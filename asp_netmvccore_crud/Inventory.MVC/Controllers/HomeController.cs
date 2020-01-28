8130bab8e89212bafdf308a7b21f6f3b51d7dbc0
shiv <shiv.singh@gomedii.com>
2020-01-28 10:43:23 +0530

    update radio,checkbox,dropdown control

--- asp_netmvccore_crud/Inventory.MVC/Controllers/HomeController.cs
+++ asp_netmvccore_crud/Inventory.MVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Inventory.MVC.Models;
 using Inventory.BusinessLib;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Inventory.MVC.Controllers
 {
@@ -20,6 +21,38 @@ namespace Inventory.MVC.Controllers
         {
             var result = operation.GetProducts();
             return View(result);
+        }
+
+        public IActionResult Create()
+        {
+            List<SelectListItem> Categories = new List<SelectListItem>();
+            Categories.Add(new SelectListItem { Text = "Electronic", Value = "Electronic" });
+            Categories.Add(new SelectListItem() { Text = "Mobile", Value = "Mobile" });
+            Categories.Add(new SelectListItem() { Text = "Men Wear", Value = "Men Wear" });
+            Categories.Add(new SelectListItem() { Text = "Woman Wear", Value = "Woman Wear" });
+            ViewBag.Categories = Categories;
+            Dictionary<string, string> colors = new Dictionary<string, string>();
+            colors.Add("Red", "Red");
+            colors.Add("Green", "Green");
+            colors.Add("Yellow", "Yellow");
+            colors.Add("Grey", "Grey");
+            ViewBag.Colors = colors;
+
+            Dictionary<string, string> Companies = new Dictionary<string, string>();
+            Companies.Add("FlipKart", "FlipKart");
+            Companies.Add("Amazon", "Amazon");
+            Companies.Add("Ebay", "Ebay");
+            Companies.Add("Relience", "Relience");
+            ViewBag.Companies = Companies;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(productVM product)
+        {
+            ModelState.Clear();
+            return View(product);
         }
 
         public IActionResult Privacy()
---
+++ asp_netmvccore_crud/Inventory.MVC/Models/productVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory.MVC.Models
+{
+    public class productVM
+    {        
+        public string ProductName { get; set; }
+        public string AvailableQty { get; set; }
+        public string Price { get; set; }
+        public string Category { get; set; }
+        public string ColorName { get; set; }        
+        public string[] Companies { get; set; }
+    }
+}
